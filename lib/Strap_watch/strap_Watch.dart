908acf4f93342316b06d39cdd5b2c597c9f03ef8
pnkaj1999 <pankajgit525@gmail.com>
2024-05-04 15:56:43 +0500

    hii

--- lib/Strap_watch/strap_Watch.dart
+++ lib/Strap_watch/strap_Watch.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:math';
+// import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -176,3 +176,5 @@ DateTime datetime = DateTime.now();
 String Day = '';
 String meridian = '';
 String month='';
+
+
